bd287ce12db98bd793289dc92520f7e843fe0e09
Matt Dickens <mpd106@gmail.com>
2014-06-02 12:57:34 +0100

    Revert debug code

--- js/app.js
+++ js/app.js
@@ -21,24 +21,7 @@ Ember.Handlebars.helper('format-date', function(date) {
 
 App.PostsRoute = Ember.Route.extend({
   model: function() {
-    return [
-  	{
-  		id: "1",
-  		title: "Third post",
-  		author: { name: "Matt" },
-  		date: new Date("06-02-2014"),
-  		excerpt: "This article is about...",
-  		body: "### Welcome\n This article is about awesome Matt's awesome hair."
-  	},
-  	{
-  		id: "2",
-  		title: "Second post",
-  		author: { name: "Matt" },
-  		date: new Date("06-01-2014"),
-  		excerpt: "This article is about...",
-  		body: "### Welcome\n This article is about awesome Matt's awesome beard."
-  	}
-  ];
+    return posts;
   }
 });
 
