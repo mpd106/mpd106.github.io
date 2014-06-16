08c03f0a0a6e69f97c04f01d2e72c06f89bde51b
Matt Dickens <mpd106@gmail.com>
2014-06-16 23:57:37 +0100

    Call jsonp api, fix annoying whitespace

--- js/app.js
+++ js/app.js
@@ -1,51 +1,34 @@
-/* global App, Ember */
+/* global App, Ember, Showdown, Handlebars, moment, $ */
 
 App = Ember.Application.create();
 
 App.Router.map(function() {
   this.resource('about');
   this.resource('posts', function() {
-  	this.resource('post', { path: ':post_id' });
+    this.resource('post', { path: ':post_id' });
   });
 });
 
 var showdown = new Showdown.converter();
 
 Ember.Handlebars.helper('format-markdown', function(body) {
-	return new Handlebars.SafeString(showdown.makeHtml(body));
+  return new Handlebars.SafeString(showdown.makeHtml(body));
 });
 
 Ember.Handlebars.helper('format-date', function(date) {
-	return moment(date).fromNow();
+  return moment(date).fromNow();
 });
 
 App.PostsRoute = Ember.Route.extend({
   model: function() {
-    return posts;
+    return $.getJSON('http://localhost:4730/posts?callback=?');
   }
 });
 
 App.PostRoute = Ember.Route.extend({
-	model: function(params) {
-		return posts.findBy('id', params.post_id);
-	}
+  model: function(params) {
+    var result = $.getJSON('http://localhost:4730/posts/' + params.post_id + '?callback=?');
+    
+    return result;
+  }
 });
-
-var posts = [
-  	{
-  		id: "1",
-  		title: "Third post",
-  		author: { name: "Matt" },
-  		date: new Date("06-02-2014"),
-  		excerpt: "This article is about...",
-  		body: "### Welcome\n This article is about awesome Matt's awesome hair."
-  	},
-  	{
-  		id: "2",
-  		title: "Second post",
-  		author: { name: "Matt" },
-  		date: new Date("06-01-2014"),
-  		excerpt: "This article is about...",
-  		body: "### Welcome\n This article is about awesome Matt's awesome beard."
-  	}
-  ];
