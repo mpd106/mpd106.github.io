60fafbb7f78d431d41b69b10497052aa9533173a
Matt Dickens <mpd106@gmail.com>
2014-06-23 22:41:24 +0100

    Add better structure, some basic styling
    The main focus of this commit is to pull the router code into router.js
    and to add a component to represent the nav, to DRY up the stuff we're
    about to add.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 /* global App, Ember, Showdown, Handlebars, moment, $, document */
 
+$(document).ready(function() {
+});
+
 App = Ember.Application.create({
   LOG_TRANSITIONS: true,
   LOG_TRANSITIONS_INTERNAL: true
-});
-
-App.Router.map(function() {
-  this.route('about');
-  this.resource('posts', function() {
-    this.resource('post', { path: ':post_id' });
-  });
 });
 
 var showdown = new Showdown.converter();
@@ -20,32 +16,6 @@ Ember.Handlebars.helper('format-markdown', function(body) {
 
 Ember.Handlebars.helper('format-date', function(date) {
   return moment(date).fromNow();
-});
-
-App.IndexRoute = Ember.Route.extend({
-  redirect: function() {
-    this.transitionTo('posts');
-  }
-});
-
-App.PostsRoute = Ember.Route.extend({
-  model: function() {
-    return $.getJSON('http://localhost:4730/posts');
-  },
-  redirect: function() {
-    var posts = this.modelFor('posts');
-    if (posts.get('length') >= 1) {
-      this.transitionTo('post', posts[0].id);
-    }
-  }
-});
-
-App.PostRoute = Ember.Route.extend({
-  model: function(params) {
-    var result = $.getJSON('http://api.mpd106.com/posts/' + params.post_id);
-    
-    return result;
-  }
 });
 
 App.PostView = Ember.View.extend({
---
+++ js/router.js
@@ -0,0 +1,28 @@
+/* globals App, Ember, $ */
+
+App.Router.map(function() {
+  this.route('about');
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' });
+  });
+});
+
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('posts');
+  }
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return $.getJSON('http://localhost:4730/posts');
+  }
+});
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    var result = $.getJSON('http://localhost:4730/posts/' + params.post_id);
+    
+    return result;
+  }
+});
